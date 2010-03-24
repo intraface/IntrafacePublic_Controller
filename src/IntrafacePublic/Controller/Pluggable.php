9e809194799279785225ef1a26a985f26ea57c27
Lars Olesen <lars@intraface.dk>
2010-03-24 20:02:48 +0000

    Moved src to trunk.

---
+++ src/IntrafacePublic/Controller/Pluggable.php
@@ -0,0 +1,42 @@
+<?php
+
+abstract class IntrafacePublic_Controller_Pluggable extends k_Controller
+{
+    
+    protected $plugins = array();
+    
+    public function triggerEvent($event, $input = NULL)
+    {
+       
+        foreach($this->getPlugins() AS $plugin) {
+            if(method_exists($plugin, $event)) {
+                $input = $plugin->$event($this, $input);
+            }
+        }
+        
+        return $input;
+    
+    }
+    
+    public function getPlugins()
+    {
+        if(!empty($this->plugins)) {
+            return $this->plugins;
+        } elseif(method_exists($this->context, 'getPlugins')) {
+            return $this->context->getPlugins();
+        }
+        
+        return array();
+    }
+    
+    public function loadPlugin($plugin)
+    {
+        $name = get_class($plugin);
+        $this->plugins[$name] = $plugin;
+    }
+    
+    
+    
+}
+
+?>
