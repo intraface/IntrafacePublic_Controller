6a174aa2407efed4e3129020615c91b1b4c7f11b
Lars Olesen <lars@intraface.dk>
2010-10-03 13:57:06 -0700

    Changed to konstrukt2

--- src/IntrafacePublic/Controller/Pluggable.php
+++ src/IntrafacePublic/Controller/Pluggable.php
@@ -1,42 +1,34 @@
 <?php
-
-abstract class IntrafacePublic_Controller_Pluggable extends k_Controller
+abstract class IntrafacePublic_Controller_Pluggable extends k_Component
 {
-    
     protected $plugins = array();
-    
+
     public function triggerEvent($event, $input = NULL)
     {
-       
-        foreach($this->getPlugins() AS $plugin) {
-            if(method_exists($plugin, $event)) {
+
+        foreach ($this->getPlugins() AS $plugin) {
+            if (method_exists($plugin, $event)) {
                 $input = $plugin->$event($this, $input);
             }
         }
-        
+
         return $input;
-    
     }
-    
+
     public function getPlugins()
     {
-        if(!empty($this->plugins)) {
+        if (!empty($this->plugins)) {
             return $this->plugins;
         } elseif(method_exists($this->context, 'getPlugins')) {
             return $this->context->getPlugins();
         }
-        
+
         return array();
     }
-    
+
     public function loadPlugin($plugin)
     {
         $name = get_class($plugin);
         $this->plugins[$name] = $plugin;
     }
-    
-    
-    
 }
-
-?>
